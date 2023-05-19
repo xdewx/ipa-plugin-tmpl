534b62307cfcf93ff155b9f484db21560f19510b
leoking <present150608@sina.com>
2023-05-19 17:18:32 +0800

    fix: set base

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/dist",
   plugins: [
     vue(), vueJsx(),
     AutoImport({
