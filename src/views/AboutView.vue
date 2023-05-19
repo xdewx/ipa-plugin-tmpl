4b2d09d058af441b78aede09ffd449cf84b00467
dew <present150608@sina.com>
2023-05-19 16:25:48 +0800

    Initialize naive

---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div class="flex flex-col">
+      <div class="bottom-2">tailwindcss</div>
+      <div class="flex-1 border">hello tailwindcss</div>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,51 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import IconsResolver from 'unplugin-icons/resolver'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(), vueJsx(),
+    AutoImport({
+      eslintrc: {
+        enabled: true,
+      },
+      imports: ['vue', 'vue-router', 'pinia', '@vueuse/core', '@vueuse/head', {
+        'naive-ui': [
+          'useDialog',
+          'useMessage',
+          'useNotification',
+          'useLoadingBar'
+        ]
+      }],
+    }),
+    Components({
+      resolvers: [
+        NaiveUiResolver(),
+        IconsResolver({
+          prefix: 'icon', // 自动引入的Icon组件统一前缀，默认为 i，设置false为不需要前缀
+          // {prefix}-{collection}-{icon} 使用组件解析器时，您必须遵循名称转换才能正确推断图标。
+          // alias: { park: 'icon-park' } 集合的别名
+          // 在这个网站找图标，然后将对应的合集名添加至此：https://icones.netlify.app/
+          enabledCollections: ["heroicons-outline", "mdi"] // 这是可选的，默认启用 Iconify 支持的所有集合['mdi']
+        })
+      ]
+    }),
+    Icons({
+      autoInstall: true,
+      compiler: "vue3",
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
